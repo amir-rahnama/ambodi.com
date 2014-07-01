95c86efcb5048a6c8b18ccfe4793dba0375ed0fc
ambodi <amir.rahnama@gmail.com>
2014-07-01 10:56:44 +0200

    update HTML and JS files for collapsing the top bar

--- js/grayscale.js
+++ js/grayscale.js
@@ -1,9 +1,9 @@
     //jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $(".navbar-fixed-desktop").addClass("top-nav-collapse");
     } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".navbar-fixed-desktop").removeClass("top-nav-collapse");
     }
 });
 
